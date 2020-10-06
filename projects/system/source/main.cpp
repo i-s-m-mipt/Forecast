ee32650f8011c8a10d8ff968f876ebc28523a640
Ivan Makarov <i.s.m.mipt@yandex.ru>
2020-10-06 17:45:57 +0300

    Функция main подготовлена для запуска в режиме учителя и ученика.

--- projects/system/source/main.cpp
+++ projects/system/source/main.cpp
@@ -1,20 +1,35 @@
+#include <exception>
+#include <fstream>
 #include <iostream>
+#include <stdexcept>
+#include <string>
 
 #include "../../shared/source/logger/logger.hpp"
 
 #include "teacher/teacher.hpp"
 
-using Logger = solution::shared::Logger;
+using Logger  = solution::shared::Logger;
+using System  = solution::system::System;
+using Teacher = solution::system::Teacher;
 
-int main(int argc, char** argv)
+int main(int argc, char ** argv)
 {
 	RUN_LOGGER(logger);
 
 	try
 	{
-		solution::system::Teacher(20U, 100U).run();
+		if (argc > 1)
+		{
+			System system(System::string_generator(std::string(argv[1])));
 
-		system("pause");
+			system.run();
+
+			save_system_deviation(system);
+		}
+		else
+		{
+			Teacher(64U, 100U).run();
+		}
 
 		return EXIT_SUCCESS;
 	}
@@ -22,15 +37,11 @@ int main(int argc, char** argv)
 	{
 		logger.write(Logger::Severity::fatal, exception.what());
 
-		system("pause");
-
 		return EXIT_FAILURE;
 	}
 	catch (...)
 	{
 		logger.write(Logger::Severity::fatal, "unknown exception");
-
-		system("pause");
 
 		return EXIT_FAILURE;
 	}
