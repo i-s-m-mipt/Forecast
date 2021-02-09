efe6ed3f205b1d65ba291a3dc239c5f10271da08
Ivan Makarov <i.s.m.mipt@yandex.ru>
2021-02-09 06:43:14 +0300

    Обновлена функция main в соответствии с новым алгоритмом.

--- projects/system/source/main.cpp
+++ projects/system/source/main.cpp
@@ -1,19 +1,14 @@
 #include <exception>
-#include <fstream>
 #include <iostream>
 #include <stdexcept>
 #include <string>
 
 #include "../../shared/source/logger/logger.hpp"
-#include "../../shared/source/python/python.hpp"
 
 #include "system/system.hpp"
-#include "teacher/teacher.hpp"
 
-using Logger  = solution::shared::Logger;
-using Python  = solution::shared::Python;
-using System  = solution::system::System;
-using Teacher = solution::system::Teacher;
+using Logger = solution::shared::Logger;
+using System = solution::system::System;
 
 int main(int argc, char ** argv)
 {
@@ -21,46 +16,9 @@ int main(int argc, char ** argv)
 
 	try
 	{
-		if (argc > 1)
-		{
-			const std::string argument(argv[1]);
+		System system;
 
-			if (argument == "-plugin")
-			{
-				System system(System::random_generator()); // model
-
-				system.run();
-				system.save();
-			}
-			else
-			{
-				if (argument == "-function")
-				{
-					if (argc - 3 == 2)
-					{
-						solution::system::apply_genetic_algorithm(argv[2], argv[3], argv[4]);
-					}
-					else
-					{
-						solution::system::apply_genetic_algorithm(argv[2], argv[3], argv[5], argv[4], argv[6]);
-					}
-				}
-				else
-				{
-					System system(System::string_generator(argument));
-
-					system.run();
-
-					Teacher::save_deviation(system);
-				}
-			}
-		}
-		else
-		{
-			Teacher(1024U).run();
-
-			system("pause");
-		}
+		system.run();
 
 		return EXIT_SUCCESS;
 	}
